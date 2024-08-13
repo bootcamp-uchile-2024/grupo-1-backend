a91b3fc8d8953df6d0c64cfa666716dc73ac5cd1
roancamo <rodrigo.cabello.1979@gmail.com>
2024-08-13 00:01:57 -0400

    version tarea 3

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,27 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-<<<<<<< HEAD
 import { UsuariosModule } from './usuarios/usuarios.module';
 import { DespachosModule } from './despachos/despachos.module';
 import { ProductosModule } from './productos/productos.module';
-//import { UsuariosModule } from './usuarios/usuarios.module';
 import { ComprasModule } from './compras/compras.module';
 import { VentasModule } from './ventas/ventas.module';
+import { EquipoModule } from './equipo/equipo.module';
 
 @Module({
-  imports: [UsuariosModule, ProductosModule, DespachosModule, ComprasModule, VentasModule],
+  imports: [UsuariosModule, ProductosModule, DespachosModule, ComprasModule, VentasModule,EquipoModule],
   controllers: [AppController],
   providers: [AppService],
-=======
-import { EquipoModule } from './equipo/equipo.module';
-import { EquipoController } from './equipo/equipo.controller';
-import { EquipoService } from './equipo/equipo.service';
-
-@Module({
-  imports: [EquipoModule],
-  controllers: [AppController,EquipoController],
-  providers: [AppService,EquipoService],
->>>>>>> equipo
-})
+ })
 export class AppModule {}
