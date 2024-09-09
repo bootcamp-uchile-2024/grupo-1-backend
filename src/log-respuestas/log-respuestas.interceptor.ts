02bb8d630eebbaeb6f958f88736b49bc92ef1aae
Christian Castillo <cnicolascastillop@gmail.com>
2024-09-09 12:02:00 -0300

    verifica funcion del interceptor

--- src/log-respuestas/log-respuestas.interceptor.ts
+++ src/log-respuestas/log-respuestas.interceptor.ts
@@ -1,9 +1,15 @@
-import { CallHandler, ExecutionContext, Injectable, NestInterceptor } from '@nestjs/common';
+import {
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  NestInterceptor,
+} from '@nestjs/common';
 import { Observable } from 'rxjs';
 
 @Injectable()
 export class LogRespuestasInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    console.log('Soy un interceptor de respuestas WORK IN PROGRESS');
     return next.handle();
   }
 }
