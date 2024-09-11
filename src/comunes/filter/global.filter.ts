ddeb8771d38d62758f677bbb4e58ea13f7cde432
roancamo <rodrigo.cabello.1979@gmail.com>
2024-09-10 22:57:23 -0300

    corrige Filter HttpException

--- src/comunes/filter/global.filter.ts
+++ src/comunes/filter/global.filter.ts
@@ -1,28 +1,44 @@
 import { ArgumentsHost, BadRequestException, Catch, ExceptionFilter, HttpException, HttpStatus, ImATeapotException } from '@nestjs/common';
 import { Response } from 'express';
+import { ErrorPlantopia } from '../error-plantopia/error-plantopia';
 @Catch()
 export class GlobalFilter<ImATeapotException> implements ExceptionFilter {
-  catch(exception: HttpException, host: ArgumentsHost) {
-    console.log('Filter Global Plantopia');
+  catch(exception: unknown, host: ArgumentsHost) {
     const ctx = host.switchToHttp();
     const response = ctx.getResponse<Response>();
     const request = ctx.getRequest<Request>();
-    //const status = exception.getStatus();/* ver tema 
-    const message = exception.message;
-    let status = HttpStatus.INTERNAL_SERVER_ERROR;
-    //let message = 'Internal server error';
-    
+
+    // Variables para manejar el código de estado y mensaje
+    let status: number;
+    let message: string;
+    console.log('Filter HttpException Inicio');
+    // Si la excepción es una instancia de HttpException (manejo de excepciones NestJS)
+    if (exception instanceof HttpException) {
+      status = exception.getStatus();
+      const errorResponse = exception.getResponse();
+      message = typeof errorResponse === 'string' ? errorResponse : (errorResponse as any).message || 'Unknown error';
+    }
+    // Si la excepción es una instancia de ErrorPlantopia (excepción personalizada)
+    else if (exception instanceof ErrorPlantopia) {
+      status = exception.statusCode; // Usamos el statusCode de ErrorPlantopia
+      message = exception.message;
+    }
+    // Si es una excepción genérica o no controlada
+    else {
+      status = HttpStatus.BAD_REQUEST;  // Usar 400 por defecto en lugar de 500
+      message = 'Ha ocurrido un error interno';
+    }
+
+    // Enviar la respuesta con el código de estado adecuado
     response.status(status).json({
-      /*queda mas mejor el  */
       statusCode: status,
-      fecha: new Date().toISOString(),
-      path: request.url,
       message: message,
-        //mensajes: exception.getResponse()["message"],
-      //error: "Error en la petición",
+      timestamp: new Date().toISOString(),
+      path: request.url,
     });
-    console.log('Filter Global PLANTOPIA Status:' + status);
-    console.log('Filter Global PLANTOPIA Mensaje del PIPE:' + message);
+  
+    console.log('Filter HttpException Global PLANTOPIA Status:' + status);
+    console.log('Filter HttpException Global PLANTOPIA Mensaje del PIPE:' + message);
   }
 
 }
